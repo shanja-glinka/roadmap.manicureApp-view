3848c99aa127bc1e7797041f78777ce5874a4027
shanja-glinka <rul3iofu1s7@outlook.com>
2023-04-04 14:38:24 +0300

    Reset paths

--- assets/frontend/js/app.js
+++ assets/frontend/js/app.js
@@ -1,9 +1,9 @@
 const Months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
 
 // const stickerRequest = '/stickers';
-const stickerRequest = './assets/var/stickers.json';
+const stickerRequest = 'assets/var/stickers.json';
 const stickerEvents = new StickerEvents;
-const defaultScriptPath = './assets/frontend/js';
+const defaultScriptPath = 'assets/frontend/js';
 const DOCUMENT_SCRIPTS_LOADS = [];
 
 
